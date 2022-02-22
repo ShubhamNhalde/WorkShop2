e1c7845faa5ec7a5f731730221251849c708a3b3
Shubham <nhaldes@gmail.com>
2022-02-22 16:44:17 +0530

    Added: My Own Auto Shop Using OOp's Concept

---
+++ Ford.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.workshopautoshop;
+
+public class Ford extends Car {
+	
+	int year;
+	int manufacturerDiscount;
+
+	public Ford(int year, int manufacturerDiscount, int speed, double regularPrice, String color) {
+		super(speed, regularPrice, color);
+		this.year = year;
+		this.manufacturerDiscount = manufacturerDiscount;
+	}
+
+	public double getSalePrice() {
+		double salePrice;
+		double price = super.getSalePrice();
+		salePrice = price - manufacturerDiscount;
+		return salePrice;
+	}
+	
+	
+
+}
